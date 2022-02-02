c3906434d71bcc84890c9baad51fe6cd6e48b8f2
beProsto <lavionperavion2@gmail.com>
2022-02-02 22:15:38 +0100

    works as a PWA

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', () => {
+	console.log('Install!');
+});
+self.addEventListener("activate", (event) => {
+	console.log('Activate!');
+});
+self.addEventListener('fetch', (event) => {
+	console.log('Fetch!', event.request);
+});
