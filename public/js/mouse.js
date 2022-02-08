93a0d95e85aab5ede382567f2a2c1f3fadf68ad3
beProsto <lavionperavion2@gmail.com>
2022-02-08 17:03:05 +0100

    should fix the problems on mobile lol

--- public/js/mouse.js
+++ public/js/mouse.js
@@ -7,7 +7,7 @@ let mouse = {
 	_touchintlpos: [0.0, 0.0]
 };
 
-window.onmousemove = (e) => { 
+window.onmousemove = (e) => {
 	mouse.position[0] = e.clientX; 
 	mouse.position[1] = e.clientY; 
 	mouse.velocity[0] = e.movementX;
@@ -17,17 +17,17 @@ window.onmousedown = (e) => { mouse.pressedButtons[e.button] = true; };
 window.onmouseup = (e) => { mouse.pressedButtons[e.button] = false; };
 window.onwheel = (e) => { mouse.wheel = e.deltaY; };
 
-window.ontouchmove = (e) => { 
-	mouse.position[0] = e.touches[0].clientX; 
-	mouse.position[1] = e.touches[0].clientY; 
+window.ontouchmove = (e) => {
+	mouse.position[0] = e.touches[0].pageX; 
+	mouse.position[1] = e.touches[0].pageY; 
 	mouse.velocity[0] = mouse.position[0] - mouse._touchintlpos[0];
 	mouse.velocity[1] = mouse.position[1] - mouse._touchintlpos[1];
 	mouse._touchintlpos[0] = mouse.position[0];
 	mouse._touchintlpos[1] = mouse.position[1];
 };
-window.ontouchstart = (e) => { 
-	mouse.position[0] = e.touches[0].clientX; 
-	mouse.position[1] = e.touches[0].clientY; 
+window.ontouchstart = (e) => {
+	mouse.position[0] = e.touches[0].pageX; 
+	mouse.position[1] = e.touches[0].pageY; 
 	mouse.velocity[0] = 0.0;
 	mouse.velocity[1] = 0.0;
 	mouse._touchintlpos[0] = mouse.position[0];
@@ -35,12 +35,12 @@ window.ontouchstart = (e) => {
 	
 	mouse.pressedButtons[0] = true; 
 };
-window.ontouchend = (e) => { 
+window.ontouchend = (e) => {
 	mouse.pressedButtons[0] = false; 
 	mouse.velocity[0] = 0.0;
 	mouse.velocity[1] = 0.0;
 };
-window.ontouchcancel = (e) => { 
+window.ontouchcancel = (e) => {
 	mouse.pressedButtons[0] = false; 
 	mouse.velocity[0] = 0.0;
 	mouse.velocity[1] = 0.0;
