c56ab8f62db23a2d26fa7f4a8704892ae2942fee
beProsto <lavionperavion2@gmail.com>
2022-02-08 20:30:12 +0100

    initialisations

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -1,5 +1,7 @@
 // Simply a list of currently pressed keys
 let pressedKeys = {};
 
-window.onkeyup = (e) => { pressedKeys[e.which] = false; };
-window.onkeydown = (e) => { pressedKeys[e.which] = true; };
+function initKeyboardHandling() {
+	window.onkeyup = (e) => { pressedKeys[e.which] = false; };
+	window.onkeydown = (e) => { pressedKeys[e.which] = true; };
+}
