2ff564480743f29562524a832021043ccb5abad4
beProsto <lavionperavion2@gmail.com>
2022-02-12 02:56:03 +0100

    added checks of where the touch / mouse cursor is on the screen (todo: rename mouse api to pointer api)

--- src/game.hpp
+++ src/game.hpp
@@ -34,7 +34,6 @@ public:
 
 		cout << "Uniforms gotten:\nu_Offset = " << u_Offset << "\nu_Size = " << u_Size << "\nu_Aspect = " << u_Aspect << endl;
 
-		glUniform3f(u_Size, 0.5f, 0.5f, 0.0f);
 	}
 
 	void update(float time) {
@@ -45,13 +44,38 @@ public:
 		glUniform1f(u_Aspect, get_width() / get_height());
 
 		// cout << "x: " << get_mouse_position_x() << " / " << get_width() << "  ||  " << "y: " << get_mouse_position_y() << " / " << get_height() << endl;
-		for(uint32_t mouseId = 0; mouseId < get_mouse_ammount(); mouseId++) {
+		// for(uint32_t mouseId = 0; mouseId < get_mouse_ammount(); mouseId++) {
+		// 	float x, y;
+			
+		// 	x = get_mouse_position_x(mouseId) / get_width() * 2.0f - 1.0f;
+		// 	y = get_mouse_position_y(mouseId) / get_height() * 2.0f - 1.0f;
+		// 	y = -y; // inverse y
+
+		// 	glUniform3f(u_Offset, x, y, 0.0f);
+		// 	glDrawArrays(GL_TRIANGLES, 0, 3);
+		// }
+		uint32_t mouseId;
+		// left side of the screen - a circle
+		if(get_mouse_id_in_area(mouseId, Circle{get_width() / 6.0f, get_height() / 2.0f, get_height()/4.0f})) {
 			float x, y;
 			
 			x = get_mouse_position_x(mouseId) / get_width() * 2.0f - 1.0f;
 			y = get_mouse_position_y(mouseId) / get_height() * 2.0f - 1.0f;
 			y = -y; // inverse y
 
+			glUniform3f(u_Size, 0.2f, 0.2f, 0.0f);
+			glUniform3f(u_Offset, x, y, 0.0f);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+		// right side of the screen - an Axis Aligned Bounding Box
+		if(get_mouse_id_in_area(mouseId, AABB{get_width() / 2.0f, 0.0f, get_width() / 2.0f, get_height()})) {
+			float x, y;
+			
+			x = get_mouse_position_x(mouseId) / get_width() * 2.0f - 1.0f;
+			y = get_mouse_position_y(mouseId) / get_height() * 2.0f - 1.0f;
+			y = -y; // inverse y
+
+			glUniform3f(u_Size, 0.5f, 0.5f, 0.0f);
 			glUniform3f(u_Offset, x, y, 0.0f);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 		}
--- src/utils/include/mouse.hpp
+++ src/utils/include/mouse.hpp
@@ -9,18 +9,58 @@ WASM_IMPORT float get_mouse_wheel();
 
 WASM_IMPORT uint32_t get_mouse_ammount();
 
-inline float is_mouse_button_pressed(uint32_t _button, uint32_t _mouseId = 0) {
-	return __wasm_import_is_mouse_button_pressed(_mouseId * 5 + _button);
+inline float is_mouse_button_pressed(uint32_t button, uint32_t mouse_id = 0) {
+	return __wasm_import_is_mouse_button_pressed(mouse_id * 5 + button);
 };
-inline float get_mouse_position_x(uint32_t _mouseId = 0) {
-	return __wasm_import_get_mouse_position(_mouseId * 2 + 0);
+inline float get_mouse_position_x(uint32_t mouse_id = 0) {
+	return __wasm_import_get_mouse_position(mouse_id * 2 + 0);
 };
-inline float get_mouse_position_y(uint32_t _mouseId = 0) {
-	return __wasm_import_get_mouse_position(_mouseId * 2 + 1);
+inline float get_mouse_position_y(uint32_t mouse_id = 0) {
+	return __wasm_import_get_mouse_position(mouse_id * 2 + 1);
 };
-inline float get_mouse_velocity_x(uint32_t _mouseId = 0) {
-	return __wasm_import_get_mouse_velocity(_mouseId * 2 + 0);
+inline float get_mouse_velocity_x(uint32_t mouse_id = 0) {
+	return __wasm_import_get_mouse_velocity(mouse_id * 2 + 0);
 };
-inline float get_mouse_velocity_y(uint32_t _mouseId = 0) {
-	return __wasm_import_get_mouse_velocity(_mouseId * 2 + 1);
+inline float get_mouse_velocity_y(uint32_t mouse_id = 0) {
+	return __wasm_import_get_mouse_velocity(mouse_id * 2 + 1);
 };
+
+struct AABB {float x; float y; float w; float h;};
+inline bool get_mouse_id_in_area(uint32_t& mouse_id_ref, AABB aabb) {
+	uint32_t mice = get_mouse_ammount();
+	for(uint32_t mid = 0; mid < mice; mid++) {
+		float x = get_mouse_position_x(mid);
+		float y = get_mouse_position_y(mid);
+		// if touch is outside the boundaries, continue on with the search
+		if(x < aabb.x || x > aabb.x + aabb.w || y < aabb.y || y > aabb.y + aabb.h) {
+			continue;
+		}
+
+		// touch inside the boundaries was found
+		mouse_id_ref = mid;
+		return true;
+	}
+	// touch inside the boundaries was not found
+	return false;
+}
+
+struct Circle {float x; float y; float r;};
+inline bool get_mouse_id_in_area(uint32_t& mouse_id_ref, Circle circle) {
+	uint32_t mice = get_mouse_ammount();
+	for(uint32_t mid = 0; mid < mice; mid++) {
+		float x = get_mouse_position_x(mid);
+		float y = get_mouse_position_y(mid);
+
+		// if touch is outside the boundaries, continue on with the search
+		float outx = x-circle.x;
+		float outy = y-circle.y;
+		if(sqrtf(outx*outx + outy*outy) > circle.r) {
+			continue;
+		}
+
+		// touch inside the boundaries was found
+		mouse_id_ref = mid;
+		return true;
+	}
+	return false;
+}
