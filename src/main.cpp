11a7c7376824e4444bab8e0003bed862cf5aadfb
beProsto <lavionperavion2@gmail.com>
2022-03-20 23:30:28 +0100

    rewrote the new and delete definitions

--- src/main.cpp
+++ src/main.cpp
@@ -60,5 +60,5 @@ WASM_EXPORT void begin_game() {
 
 // Needed for the programme to work
 WASM_EXPORT void __cxa_atexit() {}
-WASM_EXPORT void* _Znwm(size_t _s) { return malloc(_s); }
-WASM_EXPORT void _ZdlPv(void* _p) { free(_p); }
+void* operator new(size_t _s) { return malloc(_s); }
+void operator delete(void* _p) { free(_p); }
